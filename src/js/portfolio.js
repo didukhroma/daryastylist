f9543930c12c15c46bfcfca5084c93464ec42428
didukhroma <didukhroma@gmail.com>
2024-02-09 12:20:59 +0200

    portfolio: add modal

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,30 +1,9 @@
 import SimpleLightbox from 'simplelightbox';
 
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const portfolio = document.querySelector('.js-portfolio');
 const button = document.querySelector('.js-button');
-
-const markup = () => {
-  console.log('go');
-  const markDown = [];
-
-  for (let i = 1; i <= 18; i++) {
-    markDown.push(`<li  class="portfolio-list-item ${
-      i > 4 && 'visually-hidden'
-    }" data-id="${i}">
-    <div class="set-img-wrapper">
-      <img  
-      class="js-portfolio-img"    
-       srcset = './img/portfolio/img${i}.jpg 1x, ./img/portfolio/img${i}@2x.jpg 2x'
-      src="./img/portfolio/img${i}.jpg"
-       alt="hair style" 
-        loading = "lazy"/>
-    </div>
-  </li>`);
-  }
-  return markDown.join('');
-};
-
-portfolio.insertAdjacentHTML('afterbegin', markup());
 
 const settingsSimpleLightbox = {
   selector: '.js-portfolio-img',
@@ -59,4 +38,5 @@ const openModal = e => {
 };
 
 button.addEventListener('click', handleClick);
+
 portfolio.addEventListener('click', openModal);
